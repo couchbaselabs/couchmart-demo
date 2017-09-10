6e5b2c37a49532cd82c8d01f033c679a1cbc1c3e
Matt Carabine <matt.carabine@hotmail.co.uk>
2017-09-10 20:00:36 +0100

    Improve behaviour when submitting order

--- www/js/shop.js
+++ www/js/shop.js
@@ -1,21 +1,32 @@
 $( ".submit-btn" ).click(function() {
-  var active_buttons = $(".btn-product.active");
-  var orders = [];
+    var active_buttons = $(".btn-product.active");
+    var name_box = $("#name-box");
+    var orders = [];
 
-  if (active_buttons.length < 5){
-      alert("Please select 5 items before submitting");
-      return;
-  }
+    if (!name_box.val()){
+        alert("Please enter a name before submitting.");
+        return;
+    }
+    if (active_buttons.length < 5){
+        alert("Please select 5 items before submitting.");
+        return;
+    }
 
-  active_buttons.each(function (index){
-      orders.push($(this).val())
-  });
 
-  $.post( "/submit_order",
-      JSON.stringify({
-          name: $("#name-box").val(),
-          order: orders}),
-          function( data ) {
-            alert("Order submitted!!")
-          });
+    active_buttons.each(function (index){
+        orders.push($(this).val())
+    });
+
+    $.post( "/submit_order",
+        JSON.stringify({
+            name: name_box.val(),
+            order: orders}),
+        function( data ) {
+            alert("Order submitted!")
+        })
+        .fail(function () {
+            alert("Error submitting order.")
+        });
+
+    active_buttons.removeClass("active");
 });
